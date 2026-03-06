f9a6d4d208846a16c33b0dab0975ab72f09d2f90
ben360xx <benpaulekrem@gmail.com>
2026-03-05 22:43:06 -0600

    MovementRevamp(again)
    made the movement less weird and slidey, added temporary gun and main menu.

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-@export var SPEED : float = 10.0
+@export var SPEED : float = 200.0
 @export var KICKBACK : float = 600.0
 @export var JUMP_VELOCITY : float = 400.0
 @export var jump :bool = true
+@export var walljump :bool = true
 @export var gun :bool = false
 @export var fallGraV : float = 0.3
 @export var grav : float = 1
@@ -13,44 +14,62 @@ var FinalGrav: float
 
 func _ready() -> void:
 	up_direction = Vector2.UP
+	FinalGrav = grav
+
 
 func _physics_process(delta: float) -> void:
 
 	# Gravity
 	if not is_on_floor():
-		velocity += get_gravity() * delta * FinalGrav
-
 		if velocity.y > 0:
 			FinalGrav = fallGraV
 		else:
 			FinalGrav = grav
-	if velocity.x < 0:
-		anim.flip_h = true
-	elif velocity.x > 0:
-		anim.flip_h = false
-	
 
+		velocity += get_gravity() * delta * FinalGrav
+
+
+	# Sprite Flip
+	if velocity.x < 0:
+		$Sprite/Sprite.scale.x=-1
+	elif velocity.x > 0:
+		$Sprite/Sprite.scale.x=1
+
+
+	# Jump
 	if Input.is_action_just_pressed("UP") and is_on_floor() and jump:
 		velocity.y = -JUMP_VELOCITY
 		print("jump")
 
+
+	# Wall Jump (midair only, pushes away from wall)
+	if is_on_wall() and !is_on_floor() and Input.is_action_just_pressed("UP"):
+		if !walljump:
+			return
+
+		var wall_normal = get_wall_normal()
+
+		velocity.y = -JUMP_VELOCITY
+		velocity.x = wall_normal.x * SPEED * 2
+
+
 	# Horizontal movement
 	var direction := Input.get_axis("LEFT", "RIGHT")
-	if direction:
-		print(velocity.x)
-		if velocity.x < 300:
-			velocity.x += direction * SPEED
+
+	if direction != 0:
+		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if Input.is_action_just_pressed("SHOOT") and gun:
-		var mouse_direction = global_position.direction_to(get_local_mouse_position())
-		velocity = -mouse_direction * KICKBACK
-	move_and_slide()
 
-	if is_on_wall() and Input.is_action_just_pressed("WALLJUMP") and jump:
-		velocity.x += -direction * 500
-		velocity.y = -600
+
+	# Gun Kickback
+	if Input.is_action_just_pressed("SHOOT") and gun:
+		var mouse_direction = global_position.direction_to(get_global_mouse_position())
+		velocity = -mouse_direction * KICKBACK
+
+
+	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	gun = true 
+	gun = true
